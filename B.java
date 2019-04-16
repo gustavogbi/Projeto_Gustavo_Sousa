808cd519dce5d17d8bddd91804e9c0664378a894
Gustavo Rodrigues <gustavogbi@hotmail.com>
2019-04-16 17:43:49 -0300

    primeiro commit

---
+++ B.java
@@ -0,0 +1,28 @@
+public class B {
+	private int B1;
+	private float B2;
+	
+	public void MB1() {
+		System.out.println("MB1");
+	}
+	
+	public void MB2() {
+		System.out.println("MB2");
+	}
+	
+	public int getB1() {
+		return A1;
+	}
+	
+	public float getB2() {
+		return A2;
+	}
+	
+	public void setB1(int B1) {
+		this.B1 = B1;
+	}
+	
+	public void setB2(float B2) { 
+		this.B2 = B2;
+	}
+}
